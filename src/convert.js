7d671f82683695297228f758d61224c12d5822ad
Alf Eaton <eaton.alf@gmail.com>
2019-09-07 13:47:24 +0100

    Update convert.js

--- src/convert.js
+++ src/convert.js
@@ -1,17 +1,10 @@
 const convert = require('rdfa-jsonld')
 
-const frameByType = type => ({
-  '@context': {
-    '@vocab': 'http://schema.org/'
-  },
-  '@type': type
-})
-
 const episode = async pid => {
   const url = 'http://www.bbc.co.uk/programmes/' + pid
 
   const results = await convert(url, {
-    frame: frameByType('Episode'),
+    frame: { '@type': 'http://schema.org/Episode' },
     expand: true
   })
 
